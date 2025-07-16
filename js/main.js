ba2aa37b3e393c574e679d9746ec6a6ab4a8efb2
colla06 <132714511+colla06@users.noreply.github.com>
2025-07-16 22:11:58 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@ import { initAuth } from './auth.js';
 import { searchAnime, loadPost, publishPost } from './api.js';
 import { initForm, addOption } from './form.js';
 import { generateHTML, copyHTML } from './htmlGenerator.js';
-import { initBookmarks } from './utils.js';
+import { initBookmarks, initThemeToggle, showLoader } from './utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     initAuth();
     initForm();
     initBookmarks();
+    initThemeToggle();
 
-    document.getElementById('authButton').addEventListener('click', window.handleAuthClick);
-    document.getElementById('searchButton').addEventListener('click', searchAnime);
-    document.getElementById('loadPostButton').addEventListener('click', loadPost);
-    document.getElementById('publishButton').addEventListener('click', publishPost);
+    document.getElementById('authButton').addEventListener('click', () => {
+        showLoader(true);
+        window.handleAuthClick();
+    });
+    document.getElementById('searchButton').addEventListener('click', () => {
+        showLoader(true);
+        searchAnime().finally(() => showLoader(false));
+    });
+    document.getElementById('loadPostButton').addEventListener('click', () => {
+        showLoader(true);
+        loadPost().finally(() => showLoader(false));
+    });
+    document.getElementById('publishButton').addEventListener('click', () => {
+        showLoader(true);
+        publishPost().finally(() => showLoader(false));
+    });
 
     window.addOption = addOption;
     window.generateHTML = generateHTML;
