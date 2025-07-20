21c410d66339f3ce7782b2d1651d09a377b526af
colla06 <132714511+colla06@users.noreply.github.com>
2025-07-21 01:28:46 +0200

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,17 +1,2 @@
-// Mock anime data (will be updated by generator)
-let animeData = [
-  {
-    id: 1,
-    title: "Demon Slayer",
-    image: "https://cdn.myanimelist.net/images/anime/1286/99889.jpg",
-    synopsis: "Un joven lucha contra demonios para salvar a su hermana.",
-    episodes: [
-      { number: 1, title: "Episodio 1", url: "mpv://example.com/demon-slayer-ep1.mp4" }
-    ]
-  }
-];
-
-// Mock latest episodes
-let latestEpisodes = [
-  { anime: "Demon Slayer", episode: animeData[0].episodes[0] }
-];
+// Este archivo estaría vacío ya que la lógica de la página principal está en anime.js
+console.log("home.js cargado, pero las funcionalidades están en anime.js");
